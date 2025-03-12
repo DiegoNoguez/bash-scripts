0768b8e273e583bfe9d4e8ece9dc5be242befef3
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-03-11 23:05:11 -0600

    Sintaxix basica de bash y algunos ejmeplos

---
+++ 01-introduccion.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+echo "Hola Mundo"
+
+echo "Hola $1"
+
+nombre=""
+
+echo "Ingresa tu nombre"
+read nombre 
+
+read -p "Ingresa tu apellido " apellido
+
+echo "Adivinando tu nombre..."
+
+sleep 3
+
+echo "Tu nombre es $nombre $apellido"
+
+string="Es de tipo texto"
+decimales=10
+flotantes=12.22
+booleano=false
+
