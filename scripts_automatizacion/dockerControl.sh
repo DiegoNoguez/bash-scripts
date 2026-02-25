53ab4013d5d3f065b1aca98c45a911a6a89da4a1
DiegoNoguez <noguezlopezarturo@gmail.com>
2026-02-24 19:40:23 -0600

    Se inicia con el menú para controlar Docker

---
+++ scripts_automatizacion/dockerControl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+# Script para gestionar mejor los servicios de docker y docker-compose en linux sin la necesidad de 
+# aprender cada comando por separaddo
+
+echo "Geestion de DOCKER"
